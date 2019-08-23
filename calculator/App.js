3603ce958967c20bcce9832d4d7b7a675f829510
Giang Nguyen <thuygiang152@gmail.com>
2019-08-23 23:11:04 +0300

    react native testing calculator, with hook

--- calculator/App.js
+++ calculator/App.js
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
+buttonPlus = () => {
+  result = parseInt(no1) + parseInt(no2);
+  resultShow = true;
+  return result;
+};
+
+buttonMinus = () => {
+  result = parseInt(no1) - parseInt(no2);
+  resultShow = true;
+  return result;
+};
+
 export default function App() {
-  const [no1, no2, result, setText] = useState("");
+  const [no1, setNo1] = useState(0);
+  const [no2, setNo2] = useState(0);
+  let result = useState(0);
+  let resultShow = useState(false);
 
-  const buttonPlus = () => {
-    result = no1 + no2;
-  };
-
-  const buttonMinus = () => {
-    result = no1 - no2;
-  };
   return (
     <View style={styles.container}>
       <TextInput
         style={{ width: 200, borderColor: "gray", borderWidth: 1, margin: 2 }}
-        onChangeText={no1 => setText(no1)}
+        onChange={no1 => setNo1(no1)}
         value={no1}
-        keyboardType={"numeric"}
+        keyboardType="numeric"
       />
       <TextInput
         style={{ width: 200, borderColor: "gray", borderWidth: 1, margin: 2 }}
-        onChangeText={no2 => setText(no2)}
+        onChange={no2 => setNo2(no2)}
         value={no2}
-        keyboardType={"numeric"}
+        keyboardType="numeric"
       />
       <Button onPress={buttonPlus} title="+" />
       <Button onPress={buttonMinus} title="-" />
-      <Text>{result}</Text>
+      {resultShow && <Text>{result}</Text>}
     </View>
   );
 }
