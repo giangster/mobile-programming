ecd09288f948baf0cdc1fddcf3b7c8ffa5d3d16d
Giang Nguyen <thuygiang152@gmail.com>
2019-08-26 15:47:29 +0300

    number-guessing-game completed

--- number-guessing-game/App.js
+++ number-guessing-game/App.js
@@ -1,10 +1,39 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View, Alert, Button } from "react-native";
 
 const App = () => {
+  const [number, setNumber] = useState("");
+  const [result, setResult] = useState("");
+  const [count, setCount] = useState(1);
+
+  const checkNumber = () => {
+    if (number < Math.floor(Math.random() * 100) + 1) {
+      setResult(`Your guess ${number} is too low`);
+      setCount(count + 1);
+    } else if (number > Math.floor(Math.random() * 100) + 1) {
+      setResult(`Your guess ${number} is too high`);
+      setCount(count + 1);
+    } else {
+      Alert.alert(`You guessed the number in ${count} guesses`);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>hihihi</Text>
+      <Text>{result}</Text>
+      <Text>Guess number between 1-100</Text>
+      <TextInput
+        style={{
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+          margin: 2
+        }}
+        onChangeText={text => setNumber(parseInt(text))}
+        value={number}
+        keyboardType="number-pad"
+      />
+      <Button onPress={checkNumber} title="Make guess" />
     </View>
   );
 };
