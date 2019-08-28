b83e8a893c4e4fa62c0be1571f265486eba49a9b
Giang Nguyen <thuygiang152@gmail.com>
2019-08-28 09:01:43 +0300

    exercise 4 completed

--- shopping-list/App.js
+++ shopping-list/App.js
@@ -1,19 +1,62 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [shoppingItem, setItem] = useState("");
+  const [shoppingList, setList] = useState([]);
+
+  const addItem = () => {
+    setList([...shoppingList, { shoppingItem: shoppingItem }]);
+  };
+
+  const deleteList = () => {
+    setList([]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+          margin: 1
+        }}
+        onChangeText={item => setItem(item)}
+        value={shoppingItem}
+      />
+
+      <View style={styles.buttonContainer}>
+        <Button onPress={addItem} title="Add" />
+        <Button color="#FE434C" onPress={deleteList} title="Clear" />
+      </View>
+      <Text>Shopping List</Text>
+      <FlatList
+        data={shoppingList}
+        renderItem={({ item, key }) => (
+          <Text index={key}>{item.shoppingItem} </Text>
+        )}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 100,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
+  buttonContainer: { flexDirection: "row" }
 });
+
+export default App;
