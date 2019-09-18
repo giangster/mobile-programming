dc8f63e18881691989a3591cac88009b4b8a1805
Giang Nguyen <thuygiang152@gmail.com>
2019-09-18 10:32:00 +0300

    Basic functionality of recipe-finder app completed

--- recipe-finder/App.js
+++ recipe-finder/App.js
@@ -1,19 +1,84 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableWithoutFeedback,
+  Image
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [ingredient, setIngredient] = useState("");
+  const [recipes, setRecipe] = useState([]);
+
+  const findRecipe = () => {
+    const url = `http://www.recipepuppy.com/api/?i=${ingredient}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => {
+        setRecipe(responseJson.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <View>
+          {recipes ? (
+            <FlatList
+              style={{ marginLeft: "5%" }}
+              keyExtractor={item => item.id}
+              renderItem={({ item, index }) => (
+                <Text key={index}>
+                  <Image
+                    style={{ width: 50, height: 50, margin: 10 }}
+                    source={{ uri: `${item.thumbnail}` }}
+                  />
+                  {item.title}
+                </Text>
+              )}
+              data={recipes}
+            />
+          ) : null}
+        </View>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            value={ingredient}
+            placeholder="Ingredient"
+            onChangeText={ingredient => setIngredient(ingredient)}
+          />
+          <Button onPress={findRecipe} title="Find recipe"></Button>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    paddingBottom: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  textInput: {
+    width: 300,
+    height: 35,
+    fontSize: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 5,
+    marginBottom: 15
+  }
 });
+
+export default App;
